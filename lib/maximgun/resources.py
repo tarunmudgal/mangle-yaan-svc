3d62ec63c64734b05ec8139094e62295fca8006c
tarun mudgal <mtarun@vmware.com>
2020-10-08 15:23:04 +0530

    [fix]: updated MAXIMGUN_RES_API_PREFIX as per the changes made in maximgun APIs

--- lib/maximgun/resources.py
+++ lib/maximgun/resources.py
@@ -7,8 +7,8 @@ __author__ = "tarun mudgal"
 from collections import OrderedDict
 
 # MaximGun API prefix
-MAXIMGUN_RES_API_PREFIX = "/api/res"
+MAXIMGUN_RES_API_PREFIX = "/api"
 
 MAXIMGUN = OrderedDict()
-MAXIMGUN["MANGLEYAAN_CONFIG"] = "/runtest/mangle-yaan/config"
+MAXIMGUN["MANGLEYAAN_CONFIG"] = "/res/runtest/mangle-yaan/config"
 MAXIMGUN["TASK_STATUS_UPDATE"] = "/history/status"
