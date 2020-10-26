b8e39184070f93b7b2ccc5899a767655a3ec81f0
tarun mudgal <mtarun@vmware.com>
2020-10-26 23:52:41 +0530

    [add]: - added csp error codes - module_code.json and csp_common_errors.json

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ python test_runner.py tests/test_commerce_when_am_service_unavailable.py --html=
 https://pypi.org/project/pytest-html/
 https://github.com/tarunmudgal/pytest-html-reporter/tree/master/pytest_html_reporter
 https://blog.testproject.io/2020/07/15/getting-started-with-testproject-python-sdk/
+
+# ModuleCode.java formatting to module_code.json using https://sed.js.org
+--regexp-extended 's/\s*(.*?)\((.*?), (.*?), (.*?)\)/"\2": \{"name": \3, "type": "\1", "path": \4\}/'
+
+# CspCommonErrors.java formatting to csp_common_errors.json using https://sed.js.org
+--regexp-extended 's/public static final CspError (.*?) = new CspError\((.*?), (.*?)\).*/"\2": "\1",/'
--- tests/external_dependencies/test_csp_apis_when_it_not_reachable.py
+++ tests/external_dependencies/test_csp_apis_when_it_not_reachable.py
@@ -4,18 +4,19 @@
 
 __author__ = "tarun mudgal"
 
-import math
-
 import pytest
 import requests
-from src.testlib.pytest import utils
+
 from lib.csp import resources
+from src.testlib.pytest import utils
 
 LIST_NETWORK_POLICY_FILENAMES = ["preview_env_egress_commerce_service.yaml"]
 
 
 @pytest.mark.parametrize(
-    "inject_k8s_infra_fault_block_egress_traffic_for_class", LIST_NETWORK_POLICY_FILENAMES, indirect=True,
+    "inject_k8s_infra_fault_block_egress_traffic_for_class",
+    LIST_NETWORK_POLICY_FILENAMES,
+    indirect=True,
 )
 class TestCSPAPIsWhenITServiceNotReachable(object):
     """
@@ -24,13 +25,13 @@ class TestCSPAPIsWhenITServiceNotReachable(object):
 
     @pytest.mark.dependency()
     def test_estimated_charges_api_when_it_service_calls_are_blocked(
-            self, inject_k8s_infra_fault_block_egress_traffic_for_class
+        self, inject_k8s_infra_fault_block_egress_traffic_for_class
     ):
         # expected csp api response (status_code)
         expected_status_code = 500
         if (
-                inject_k8s_infra_fault_block_egress_traffic_for_class
-                == "preview_env_egress_commerce_service.yaml"
+            inject_k8s_infra_fault_block_egress_traffic_for_class
+            == "preview_env_egress_commerce_service.yaml"
         ):
             expected_status_code = 500
 
@@ -46,18 +47,19 @@ class TestCSPAPIsWhenITServiceNotReachable(object):
         resp = cclient.make_call("GET", api_resource, retry_count=0, disable_implicit_retry=True)
 
         # verify csp api returns expected_status_code
-        assert resp.status_code == expected_status_code, "Commerce service did not return expected status_code={}".format(
-            expected_status_code)
+        assert (
+            resp.status_code == expected_status_code
+        ), "Commerce service did not return expected status_code={}".format(expected_status_code)
 
     @pytest.mark.dependency()
     def test_offers_api_when_it_service_calls_are_blocked(
-            self, inject_k8s_infra_fault_block_egress_traffic_for_class
+        self, inject_k8s_infra_fault_block_egress_traffic_for_class
     ):
         # expected csp api response (status_code)
         expected_status_code = 504
         if (
-                inject_k8s_infra_fault_block_egress_traffic_for_class
-                == "preview_env_egress_commerce_service.yaml"
+            inject_k8s_infra_fault_block_egress_traffic_for_class
+            == "preview_env_egress_commerce_service.yaml"
         ):
             expected_status_code = 504
 
@@ -67,21 +69,24 @@ class TestCSPAPIsWhenITServiceNotReachable(object):
         )
         request_body = {"billingEngine": "SAP"}
 
-        resp = cclient.make_call("POST", api_resource, json=request_body, retry_count=0, disable_implicit_retry=True)
+        resp = cclient.make_call(
+            "POST", api_resource, json=request_body, retry_count=0, disable_implicit_retry=True
+        )
 
         # verify csp api returns expected_status_code
-        assert resp.status_code == expected_status_code, "Commerce service did not return expected status_code={}".format(
-            expected_status_code)
+        assert (
+            resp.status_code == expected_status_code
+        ), "Commerce service did not return expected status_code={}".format(expected_status_code)
 
     @pytest.mark.dependency()
     def test_org_payment_methods_api_when_it_service_calls_are_blocked(
-            self, inject_k8s_infra_fault_block_egress_traffic_for_class
+        self, inject_k8s_infra_fault_block_egress_traffic_for_class
     ):
         # expected csp api response (status_code)
         expected_status_code = 500
         if (
-                inject_k8s_infra_fault_block_egress_traffic_for_class
-                == "preview_env_egress_commerce_service.yaml"
+            inject_k8s_infra_fault_block_egress_traffic_for_class
+            == "preview_env_egress_commerce_service.yaml"
         ):
             expected_status_code = 500
 
@@ -93,18 +98,19 @@ class TestCSPAPIsWhenITServiceNotReachable(object):
         resp = cclient.make_call("GET", api_resource, retry_count=0, disable_implicit_retry=True)
 
         # verify csp api returns expected_status_code
-        assert resp.status_code == expected_status_code, "Commerce service did not return expected status_code={}".format(
-            expected_status_code)
+        assert (
+            resp.status_code == expected_status_code
+        ), "Commerce service did not return expected status_code={}".format(expected_status_code)
 
     @pytest.mark.dependency()
     def test_user_payment_methods_api_when_it_service_calls_are_blocked(
-            self, inject_k8s_infra_fault_block_egress_traffic_for_class
+        self, inject_k8s_infra_fault_block_egress_traffic_for_class
     ):
         # expected csp api response (status_code)
         expected_status_code = 500
         if (
-                inject_k8s_infra_fault_block_egress_traffic_for_class
-                == "preview_env_egress_commerce_service.yaml"
+            inject_k8s_infra_fault_block_egress_traffic_for_class
+            == "preview_env_egress_commerce_service.yaml"
         ):
             expected_status_code = 500
 
@@ -116,18 +122,19 @@ class TestCSPAPIsWhenITServiceNotReachable(object):
         resp = cclient.make_call("GET", api_resource, retry_count=0, disable_implicit_retry=True)
 
         # verify csp api returns expected_status_code
-        assert resp.status_code == expected_status_code, "Commerce service did not return expected status_code={}".format(
-            expected_status_code)
+        assert (
+            resp.status_code == expected_status_code
+        ), "Commerce service did not return expected status_code={}".format(expected_status_code)
 
     @pytest.mark.dependency()
     def test_promotions_api_when_it_service_calls_are_blocked(
-            self, inject_k8s_infra_fault_block_egress_traffic_for_class
+        self, inject_k8s_infra_fault_block_egress_traffic_for_class
     ):
         # expected csp api response (status_code)
         expected_status_code = 500
         if (
-                inject_k8s_infra_fault_block_egress_traffic_for_class
-                == "preview_env_egress_commerce_service.yaml"
+            inject_k8s_infra_fault_block_egress_traffic_for_class
+            == "preview_env_egress_commerce_service.yaml"
         ):
             expected_status_code = 500
 
@@ -139,18 +146,19 @@ class TestCSPAPIsWhenITServiceNotReachable(object):
         resp = cclient.make_call("GET", api_resource, retry_count=0, disable_implicit_retry=True)
 
         # verify csp api returns expected_status_code
-        assert resp.status_code == expected_status_code, "Commerce service did not return expected status_code={}".format(
-            expected_status_code)
+        assert (
+            resp.status_code == expected_status_code
+        ), "Commerce service did not return expected status_code={}".format(expected_status_code)
 
     @pytest.mark.dependency()
     def test_list_subscriptions_api_when_it_service_calls_are_blocked(
-            self, inject_k8s_infra_fault_block_egress_traffic_for_class
+        self, inject_k8s_infra_fault_block_egress_traffic_for_class
     ):
         # expected csp api response (status_code)
         expected_status_code = 504
         if (
-                inject_k8s_infra_fault_block_egress_traffic_for_class
-                == "preview_env_egress_commerce_service.yaml"
+            inject_k8s_infra_fault_block_egress_traffic_for_class
+            == "preview_env_egress_commerce_service.yaml"
         ):
             expected_status_code = 504
 
@@ -158,11 +166,14 @@ class TestCSPAPIsWhenITServiceNotReachable(object):
         api_resource = resources.COMMERCE.get("LIST_SUBSCRIPTIONS")
         params = {"orgId": myconfig.get("csp").get("defaultOrg").get("id")}
 
-        resp = cclient.make_call("GET", api_resource, params=params, retry_count=0, disable_implicit_retry=True)
+        resp = cclient.make_call(
+            "GET", api_resource, params=params, retry_count=0, disable_implicit_retry=True
+        )
 
         # verify csp api returns expected_status_code
-        assert resp.status_code == expected_status_code, "Commerce service did not return expected status_code={}".format(
-            expected_status_code)
+        assert (
+            resp.status_code == expected_status_code
+        ), "Commerce service did not return expected status_code={}".format(expected_status_code)
 
     @pytest.mark.dependency(
         # depends=utils.get_testcase_names(
@@ -173,22 +184,24 @@ class TestCSPAPIsWhenITServiceNotReachable(object):
     # @pytest.mark.skipif(mycache["test_csp_apis_when_it_not_reachable_subscriptionId"].get("totalResults") <= 0,
     #                     reason="seems there are no subscriptionIds found")
     def test_get_subscription_api_when_it_service_calls_are_blocked(
-            self, inject_k8s_infra_fault_block_egress_traffic_for_class
+        self, inject_k8s_infra_fault_block_egress_traffic_for_class
     ):
         # expected csp api response (status_code)
         expected_status_code = 504
         if (
-                inject_k8s_infra_fault_block_egress_traffic_for_class
-                == "preview_env_egress_commerce_service.yaml"
+            inject_k8s_infra_fault_block_egress_traffic_for_class
+            == "preview_env_egress_commerce_service.yaml"
         ):
             expected_status_code = 504
 
         # make csp api call
         api_resource = resources.COMMERCE.get("GET_SUBSCRIPTION").format(
-            subscriptionId="107b5ede-470c-4fd7-9546-de57966aca84")
+            subscriptionId="107b5ede-470c-4fd7-9546-de57966aca84"
+        )
 
         resp = cclient.make_call("GET", api_resource, retry_count=0, disable_implicit_retry=True)
 
         # verify csp api returns expected_status_code
-        assert resp.status_code == expected_status_code, "Commerce service did not return expected status_code={}".format(
-            expected_status_code)
+        assert (
+            resp.status_code == expected_status_code
+        ), "Commerce service did not return expected status_code={}".format(expected_status_code)
