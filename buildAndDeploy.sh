b84c468daff54860a7a615279a394858f3ae721a
tarun mudgal <mtarun@vmware.com>
2021-01-25 16:57:28 +0530

    [update]: updated buildAndDeploy.sh

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,3 +1,3 @@
-docker login -u csp-qe-rw-token -p "$TOKEN" vmwaresaas.jfrog.io
+docker login -u $APIUSER -p $APIKEY vmwaresaas.jfrog.io
 docker build -t vmwaresaas.jfrog.io/csp/mangle-yaan:latest -f dockerfiles/Dockerfile .
 docker push vmwaresaas.jfrog.io/csp/mangle-yaan:latest
