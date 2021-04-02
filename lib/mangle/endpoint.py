f9aeb0d1da53a0ddbeb5120372e399cd4236140c
tarun mudgal <mtarun@vmware.com>
2021-04-03 00:50:58 +0530

    [Fix]: Added an improvement to update mangle endpoint credentials once it's updated in codebase - Refactored Mangle EndPoint creation logic and removed it from test_runner.py

--- lib/mangle/endpoint.py
+++ lib/mangle/endpoint.py
@@ -2,9 +2,15 @@
 Copyright 2017 VMware, Inc. All rights reserved. -- VMware confidential
 """
 import os
+import sys
 
 from lib.common import utils
-from lib.mangle import resources
+from lib.mangle import mangle_client, resources
+
+
+endpoint_cred_obj = None
+endpoint_obj = None
+test_connection_obj = None
 
 
 class EndPointBase:
@@ -91,13 +97,36 @@ class EndpointCredential(EndPointBase):
         )
         files = [("kubeConfig", open(kubeconfig_filepath, "rb"))]
         params = {"id": credential_name, "name": credential_name}
+        api_resource = resources.EP_CTRLR.get("K8S_CREDENTIALS")
+
+        return self.mangle_client.make_call(
+            "POST", api_resource, params=params, files=files, data={}
+        )
+
+    @utils.verify_status()
+    def update_credential_k8s_cluster(self, credential_name, kubeconfig_filename):
+        """
+        Creates endpoint credentials for kubernetes cluster
+        :return:
+        """
+        kubeconfig_filepath = (
+            ROOT_DIR
+            + os.path.sep
+            + "config"
+            + os.path.sep
+            + "kubeconfigs"
+            + os.path.sep
+            + kubeconfig_filename
+        )
+        files = [("kubeConfig", open(kubeconfig_filepath, "rb"))]
+        params = {"id": credential_name, "name": credential_name}
         headers = {"Authorization": "Basic YWRtaW5AbWFuZ2xlLmxvY2FsOmFkbWlu"}
         api_resource = resources.EP_CTRLR.get("K8S_CREDENTIALS")
         # return self.mangle_client.make_call("POST", resources.EP_CTRLR.get('K8S_CREDENTIALS'),
         #                                     files=multipart_form_data, params=params)
 
         return self.mangle_client.make_call(
-            "POST", api_resource, params=params, headers=headers, files=files, data={}
+            "PUT", api_resource, params=params, headers=headers, files=files, data={}
         )
 
     @utils.verify_status()
@@ -158,3 +187,212 @@ class TestConnection(EndPointBase):
         return self.mangle_client.make_call(
             "POST", resources.EP_CTRLR.get("TEST_CONNECTION"), params=params
         )
+
+
+def _create_credentials_k8s(mclient: mangle_client.MangleClient):
+    global endpoint_cred_obj
+    if endpoint_cred_obj is None:
+        endpoint_cred_obj = EndpointCredential(mclient)
+
+    does_cred_exist = False
+    status, response = endpoint_cred_obj.list_credentials()
+    if not status:
+        mylog.error("Failed to fetch credentials from mangle")
+        sys.exit(2)
+
+    for cred in response.json:
+        if cred.get("name") == myconfig.get("k8sCluster").get("credentialName"):
+            does_cred_exist = True
+            break
+    if not does_cred_exist:
+        status, response = endpoint_cred_obj.create_credential_k8s_cluster(
+            myconfig.get("k8sCluster").get("credentialName"),
+            myconfig.get("k8sCluster").get("kubeConfigFileName"),
+        )
+        if not status:
+            mylog.error(
+                "Failed to create k8s cluster credential for credentialName={}, kubeConfigFileName={}".format(
+                    myconfig.get("k8sCluster").get("credentialName"),
+                    myconfig.get("k8sCluster").get("kubeConfigFileName"),
+                )
+            )
+            sys.exit(2)
+        mylog.info(
+            "credential '{}' for k8s cluster created successfully".format(
+                myconfig.get("k8sCluster").get("credentialName")
+            )
+        )
+    else:
+        mylog.info(
+            "credential '{}' for k8s cluster already exist".format(
+                myconfig.get("k8sCluster").get("credentialName")
+            )
+        )
+
+
+def _update_credentials_k8s(mclient: mangle_client.MangleClient):
+    global endpoint_cred_obj
+    if endpoint_cred_obj is None:
+        endpoint_cred_obj = EndpointCredential(mclient)
+
+    does_cred_exist = False
+    status, response = endpoint_cred_obj.list_credentials()
+    if not status:
+        mylog.error("Failed to fetch credentials from mangle")
+        sys.exit(2)
+
+    for cred in response.json:
+        if cred.get("name") == myconfig.get("k8sCluster").get("credentialName"):
+            does_cred_exist = True
+            break
+    if does_cred_exist:
+        status, response = endpoint_cred_obj.update_credential_k8s_cluster(
+            myconfig.get("k8sCluster").get("credentialName"),
+            myconfig.get("k8sCluster").get("kubeConfigFileName"),
+        )
+        if not status:
+            mylog.error(
+                "Failed to update k8s cluster credential for credentialName={}, kubeConfigFileName={}".format(
+                    myconfig.get("k8sCluster").get("credentialName"),
+                    myconfig.get("k8sCluster").get("kubeConfigFileName"),
+                )
+            )
+            sys.exit(2)
+        mylog.info(
+            "credential '{}' for k8s cluster updated successfully".format(
+                myconfig.get("k8sCluster").get("credentialName")
+            )
+        )
+    else:
+        mylog.info(
+            "credential '{}' for k8s cluster does not exist".format(
+                myconfig.get("k8sCluster").get("credentialName")
+            )
+        )
+
+
+def _delete_credentials_k8s(mclient: mangle_client.MangleClient):
+    global endpoint_cred_obj
+    if endpoint_cred_obj is None:
+        endpoint_cred_obj = EndpointCredential(mclient)
+
+    does_cred_exist = False
+    status, response = endpoint_cred_obj.list_credentials()
+    if not status:
+        mylog.error("Failed to fetch credentials from mangle")
+        sys.exit(2)
+
+    for cred in response.json:
+        if cred.get("name") == myconfig.get("k8sCluster").get("credentialName"):
+            does_cred_exist = True
+            break
+    if does_cred_exist:
+        status, response = endpoint_cred_obj.delete_credential(
+            myconfig.get("k8sCluster").get("credentialName")
+        )
+        if not status:
+            mylog.error(
+                "Failed to delete k8s cluster credential for credentialName={}".format(
+                    myconfig.get("k8sCluster").get("credentialName")
+                )
+            )
+            sys.exit(2)
+        mylog.info(
+            "credential '{}' for k8s cluster deleted successfully".format(
+                myconfig.get("k8sCluster").get("credentialName")
+            )
+        )
+    else:
+        mylog.info(
+            "credential '{}' for k8s cluster does not exist".format(
+                myconfig.get("k8sCluster").get("credentialName")
+            )
+        )
+
+
+def _create_endpoint_k8s(mclient: mangle_client.MangleClient):
+    global endpoint_obj
+    if endpoint_obj is None:
+        endpoint_obj = Endpoint(mclient)
+
+    does_endpoint_exist = False
+    status, response = endpoint_obj.list_endpoints_k8s_cluster()
+    if not status:
+        mylog.error("Failed to fetch endpoints from mangle")
+        sys.exit(2)
+    for ep in response.json:
+        if ep.get("name") == myconfig.get("k8sCluster").get("endpointName"):
+            does_endpoint_exist = True
+            break
+    if not does_endpoint_exist:
+        try:
+            status, response = endpoint_obj.create_endpoint_k8s_cluster(
+                myconfig.get("k8sCluster").get("endpointName"),
+                myconfig.get("k8sCluster").get("credentialName"),
+                myconfig.get("k8sCluster").get("namespace"),
+            )
+        except Exception as fault:
+            # fallback for updating credentials as mostly exception occurs due to incorrect credentials
+            _update_credentials_k8s(mclient)
+            status, response = endpoint_obj.create_endpoint_k8s_cluster(
+                myconfig.get("k8sCluster").get("endpointName"),
+                myconfig.get("k8sCluster").get("credentialName"),
+                myconfig.get("k8sCluster").get("namespace"),
+            )
+        if not status:
+            mylog.error(
+                "Failed to create k8s cluster endpoint for endpointName={}, credentialName={}, namespace={}".format(
+                    myconfig.get("k8sCluster").get("endpointName"),
+                    myconfig.get("k8sCluster").get("credentialName"),
+                    myconfig.get("k8sCluster").get("namespace"),
+                )
+            )
+            sys.exit(2)
+        mylog.info(
+            "endpoint '{}' for k8s cluster created successfully".format(
+                myconfig.get("k8sCluster").get("endpointName")
+            )
+        )
+    else:
+        mylog.info(
+            "Endpoint '{}' for k8s cluster already exist".format(
+                myconfig.get("k8sCluster").get("endpointName")
+            )
+        )
+
+
+def _test_endpoint_k8s(mclient: mangle_client.MangleClient):
+    global test_connection_obj
+    if test_connection_obj is None:
+        test_connection_obj = TestConnection(mclient)
+
+    try:
+        status, response = test_connection_obj.test_endpoint(
+            myconfig.get("k8sCluster").get("endpointName")
+        )
+    except Exception as fault:
+        # fallback for updating credentials as mostly exception occurs due to incorrect credentials
+        _update_credentials_k8s(mclient)
+        status, response = test_connection_obj.test_endpoint(
+            myconfig.get("k8sCluster").get("endpointName")
+        )
+
+    if not status:
+        mylog.error(
+            "Test connection for endpoint {} failed".format(
+                myconfig.get("k8sCluster").get("endpointName")
+            )
+        )
+        sys.exit(2)
+    else:
+        mylog.info(
+            "Test connection for endpoint {} passed".format(
+                myconfig.get("k8sCluster").get("endpointName")
+            )
+        )
+
+
+def create_and_test_mangle_endpoint(mclient: mangle_client.MangleClient):
+    _create_credentials_k8s(mclient)
+    _create_endpoint_k8s(mclient)
+    _test_endpoint_k8s(mclient)
